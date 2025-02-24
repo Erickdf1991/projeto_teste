3cbb87bb3534b3a40880e006c26779cb03907300
Erick Fernandes <132783629+Erickdf1991@users.noreply.github.com>
2025-02-24 11:24:36 -0300

    Arquivo para reprodução do ambiente virtual

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -7,7 +7,7 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
-DADOS_ORIGINAIS = PASTA_DADOS / "diabetes_binary_5050split_health_indicators_BRFSS2015.csv"
+DADOS_ORIGINAIS = PASTA_DADOS / "diabetes.zip"
 DADOS_TRATADOS = PASTA_DADOS / "diabetes_tratado.parquet"
 
 # coloque abaixo outros caminhos que você julgar necessário
